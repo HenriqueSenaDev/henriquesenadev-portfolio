e6d42adb37b4f25b869dec56ac1d9274e5b265ee
HenriqueSenaDev <acopsenadeveloper@gmail.com>
2024-01-11 21:21:12 -0300

    hotfix: tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,10 +4,18 @@ const config: Config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/sections/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        light: '0px 3.197px 3.197px 0px rgba(0, 0, 0, 0.25)',
+      },
+      colors: {
+        'light-gray': 'rgba(201,201,201,0.3)',
+      },
+    },
   },
   plugins: [],
 };
